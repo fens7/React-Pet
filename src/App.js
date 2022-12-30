0594cf55cba75d47b4d26ed88783a43b54fdadf3
Maxym Zavadovych <zavadovich17@gmail.com>
2022-12-30 21:22:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                         <title />
                         <path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
                     </svg>
-                    <img src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif'/>
+                    <img alt='' src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif'/>
                 </div>
             </div>
         </div>
